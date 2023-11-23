8cb7d2037a88d43cf2ccf7041032cd94e1b94649
DronTS <109414199+DronTC@users.noreply.github.com>
2023-11-23 19:43:48 +0300

    refactor authorization and registation fragments

---
+++ app/src/main/java/com/example/housingguru/MainMenuFragment.kt
@@ -0,0 +1,27 @@
+package com.example.housingguru
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainMenuFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainMenuFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        var view = inflater.inflate(R.layout.fragment_main_menu, container, false)
+
+        return view
+    }
+}
